2dbe9f7e36dcce74e4283c9cb773b939fe77abb5
github username <github email address>
2021-07-08 18:04:21 +0545

    commit tree 696851f910861aab8f9d6855d8bfc25a01a9d473

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+    def name=(name)
+        #method to write name
+        @name = name
+    end
+    def name
+        #method to read name
+        @name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+    def breed
+        @breed
+    end
+end
